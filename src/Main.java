e8f68e4f1e2746f35961b2cb0c07da3ade783c8c
B Mousa <mbdmbader@gmail.com>
2025-02-02 21:41:30 -0500

    this is a conf

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {]
         for (int i=o ; i<10; i++){}
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Helpoiuytworld!");
         //this is the learning process
         //fghj
 
